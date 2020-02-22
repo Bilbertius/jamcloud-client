935364f3c94afa72d7dd3711452b0e484e68e910
Bilbertius <lauchlinmacneil@gmail.com>
2020-02-22 01:40:50 -0700

    Fix deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 
 export default {
-	API_ENDPOINT: 'postgres://fcbacxxyntxurp:245330dcb5943b50dcb1f91bac0c53fb0a77088f1bfdf92bc35647bdd1b2f8d5@ec2-23-22-156-110.compute-1.amazonaws.com:5432/dds99fhps7t6al',
+	API_ENDPOINT: 'https://infinite-caverns-48926.herokuapp.com/',
 	API_KEY: process.env.REACT_APP_API_KEY
 }
