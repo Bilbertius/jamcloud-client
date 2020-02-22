5c26c23242f0f7f1250c1f427f716a016d0b7d35
Bilbertius <lauchlinmacneil@gmail.com>
2020-02-22 09:56:35 -0700

    Attempt fix of broken build

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 
 export default {
-	API_ENDPOINT: 'https://infinite-caverns-48926.herokuapp.com/',
+	API_ENDPOINT: 'https://fierce-sands-64093.herokuapp.com',
 	API_KEY: process.env.REACT_APP_API_KEY
 }
