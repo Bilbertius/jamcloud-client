e31b12be962c764402d2e38b6649a0c6107837c5
Bilbertius <lauchlinmacneil@gmail.com>
2020-02-21 23:51:26 -0700

    sfe

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 
 export default {
-	API_ENDPOINT: 'https://morning-journey-68694.herokuapp.com/api',
+	API_ENDPOINT: 'postgres://fcbacxxyntxurp:245330dcb5943b50dcb1f91bac0c53fb0a77088f1bfdf92bc35647bdd1b2f8d5@ec2-23-22-156-110.compute-1.amazonaws.com:5432/dds99fhps7t6al',
 	API_KEY: process.env.REACT_APP_API_KEY
 }
