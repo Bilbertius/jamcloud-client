64243aabb3751dc5560e0087a489c6df23b7c4b8
Bilbertius <lauchlinmacneil@gmail.com>
2020-02-22 10:01:39 -0700

    Add new api_endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
 
 export default {
-	API_ENDPOINT: 'https://fierce-sands-64093.herokuapp.com',
+	API_ENDPOINT: 'https://salty-savannah-50195.herokuapp.coma/api',
 	API_KEY: process.env.REACT_APP_API_KEY
 }
