eeb0e63748a4dcef3265efab3705a623682e6199
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-10 03:26:01 -0600

    messgae

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@
 export default {
 	API_BASE_URL: 'https://damp-eyrie-72462.herokuapp.com/api',
 	JWT_SECRET: process.env.REACT_APP_JWT_SECRET,
-	API_KEY: 'pr45$DEFgh2sdp235gh4-m9egnml-rtbr@#$'
+	API_KEY: process.env.API_KEY
 }
