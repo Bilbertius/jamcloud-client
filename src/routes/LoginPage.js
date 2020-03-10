365fb086c2fa333228112dbe7ae5cc1e56434ccb
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-10 01:44:40 -0600

    fsdefs

--- src/routes/LoginPage.js
+++ src/routes/LoginPage.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import LoginForm from '../components/LoginForm/LoginForm';
-import { Section } from '../components/Utils/Utils';
+import React from "react";
+import LoginForm from "../components/LoginForm/LoginForm";
+import { Section } from "../components/Utils/Utils";
 
 export default class LoginPage extends React.Component {
-	static defaultProps = {
-		location: {},
-		history: {
-			push: () => {},
-		},
-	};
-	
-	handleLoginSuccess = () => {
-		const { location, history } = this.props;
-		const destination = (location.state || {}).from || '/songs';
-		history.push(destination)
-	};
-	
-	render() {
-		return (
-			<Section className='LoginPage'>
-				<LoginForm
-					onLoginSuccess={this.handleLoginSuccess}
-				/>
-			</Section>
-		)
-	}
+  static defaultProps = {
+    location: {},
+    history: {
+      push: () => {}
+    }
+  };
+
+  handleLoginSuccess = () => {
+    const { location, history } = this.props;
+    const destination = (location.state || {}).from || "/songs";
+    history.push(destination);
+  };
+
+  render() {
+    return (
+      <Section className="LoginPage">
+        <LoginForm onLoginSuccess={this.handleLoginSuccess} />
+      </Section>
+    );
+  }
 }
